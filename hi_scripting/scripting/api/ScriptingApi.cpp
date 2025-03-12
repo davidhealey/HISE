8469fb80d391697aac274f3b7613a014f6f9a627
dave <dave41264@hotmail.com>
2025-03-12 21:51:21 +0000

    browseInternally now returns an empty file if no valid file is selected.

--- hi_scripting/scripting/api/ScriptingApi.cpp
+++ hi_scripting/scripting/api/ScriptingApi.cpp
@@ -7396,10 +7396,13 @@ void ScriptingApi::FileSystem::browseInternally(File f, bool forSaving, bool isD
 				a = var(new ScriptingObjects::ScriptFile(p_, fc.getResult()));
 		}
 
-		if (a.isObject())
+		if (!a.isObject())
 		{
-			wc.call(&a, 1);
+			File emptyFile;
+			a = var(new ScriptingObjects::ScriptFile(p_, emptyFile));
 		}
+			
+		wc.call(&a, 1);
 		
 		fileChooserIsOpen = false;
 	};
