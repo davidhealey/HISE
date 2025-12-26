9ca767f1015405b0de6829101ceaaccb49c7eeea
dave <dave41264@hotmail.com>
2025-12-26 01:01:56 +0000

    Initialise full expansion value tree

--- hi_scripting/scripting/api/ScriptExpansion.cpp
+++ hi_scripting/scripting/api/ScriptExpansion.cpp
@@ -2510,6 +2510,8 @@ juce::Result FullInstrumentExpansion::initialise()
 		if(iconData.isNotEmpty())
 			pool->getImagePool().setDataProvider(new PublicIconProvider(&pool->getImagePool(), iconData));
 
+		initialiseFromValueTree(allData);
+
 		fullyLoaded = false;
 
 		getMainController()->getExpansionHandler().addListener(this);
