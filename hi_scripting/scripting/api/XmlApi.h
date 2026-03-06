bad45bf6fa766e6884d9f8e2b618c8cb9223c812
Christoph Hart <christoph-hart@gmx.de>
2026-03-06 10:04:05 +0100

    - updated API docs

--- hi_scripting/scripting/api/XmlApi.h
+++ hi_scripting/scripting/api/XmlApi.h
@@ -6,7 +6,7 @@
 namespace XmlApi
 {
     extern const char*  apivaluetree_dat;
-    const int           apivaluetree_datSize = 319253;
+    const int           apivaluetree_datSize = 359121;
 }
 
 #endif
